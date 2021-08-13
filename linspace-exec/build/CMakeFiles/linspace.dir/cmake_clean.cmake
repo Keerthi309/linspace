59f30a5db2ad463d71ba935cc63722aa3cd8ab15
root <root@DESKTOP-SUGL1FB.localdomain>
2021-08-13 21:03:45 +0530

    first commit

---
+++ linspace-exec/build/CMakeFiles/linspace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linspace.dir/linspace.cpp.o"
+  "linspace"
+  "linspace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linspace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
